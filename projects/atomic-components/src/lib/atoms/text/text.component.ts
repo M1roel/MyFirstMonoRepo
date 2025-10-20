25fdb5d22632cc8a4787e3905a2db0181d61b5f9
M1roel <ppfautsch@yahoo.de>
2025-10-20 16:36:01 +0200

    feat: update text component to accept dynamic input for text rendering

--- projects/atomic-components/src/lib/atoms/text/text.component.ts
+++ projects/atomic-components/src/lib/atoms/text/text.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'atomic-text',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './text.component.scss'
 })
 export class TextComponent {
-
+  @Input() text: string = '';
 }
