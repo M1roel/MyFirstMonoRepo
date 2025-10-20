bae62e5886ff6ff116be10817216bfd22d87793b
M1roel <ppfautsch@yahoo.de>
2025-10-20 18:30:59 +0200

    refactor: update headline color and adjust styles
    - Changed headline color from v.$wa-color-neutral-900 to v.$wa-color-neutral-0 in headline.component.scss.
    - Simplified app.component.html by removing placeholder content and retaining only necessary components.
    - Updated app.component.ts to import SearchBarComponent and InfoCardComponent for better structure.
    - Modified styles.scss to use v.$wa-color-neutral-900 for body background and adjusted padding for better layout.

--- projects/weather-app/src/app/app.component.ts
+++ projects/weather-app/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TextComponent } from '../../../atomic-components/src/lib/atoms/text/text.component';
+import { HeadlineComponent } from '../../../atomic-components/src/lib/atoms/headline/headline.component';
+import { SearchBarComponent } from '../../../atomic-components/src/lib/molecules/search-bar/search-bar.component';
+import { InfoCardComponent } from '../../../atomic-components/src/lib/molecules/info-card/info-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InfoCardComponent, HeadlineComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
