21023317e2fbe549c6514ce469e82940f5e8e96a
M1roel <ppfautsch@yahoo.de>
2025-10-28 10:39:40 +0100

    feat: add icon support to InfoCard component and update styles

--- projects/atomic-components/src/lib/molecules/info-card/info-card.component.ts
+++ projects/atomic-components/src/lib/molecules/info-card/info-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TextComponent } from '../../atoms/text/text.component';
+import { IconComponent } from "../../atoms/icon/icon.component";
 
 @Component({
   selector: 'atomic-info-card',
-  imports: [TextComponent],
+  imports: [TextComponent, CommonModule , IconComponent],
   templateUrl: './info-card.component.html',
   styleUrl: './info-card.component.scss'
 })
@@ -11,4 +13,5 @@ export class InfoCardComponent {
   @Input() label: string = '';
   @Input() value: string = '';
   @Input() unit: string = '';
+  @Input() icon?: string;
 }
