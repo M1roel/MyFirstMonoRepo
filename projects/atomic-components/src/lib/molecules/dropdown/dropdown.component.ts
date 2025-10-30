00a1951a89a39afc94b205775a836001d28dcf4c
M1roel <ppfautsch@yahoo.de>
2025-10-30 16:35:06 +0100

    feat: implement Dropdown component with weekday display; update daily forecast template to use ngFor syntax

--- projects/atomic-components/src/lib/molecules/dropdown/dropdown.component.ts
+++ projects/atomic-components/src/lib/molecules/dropdown/dropdown.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { TextComponent, IconComponent } from '../../../public-api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'atomic-dropdown',
-  imports: [],
+  imports: [TextComponent, IconComponent, CommonModule],
   templateUrl: './dropdown.component.html',
   styleUrl: './dropdown.component.scss'
 })
 export class DropdownComponent {
+  
+  weekdays: string[] = [
+    'Montag',
+    'Dienstag', 
+    'Mittwoch',
+    'Donnerstag',
+    'Freitag',
+    'Samstag',
+    'Sonntag'
+  ];
 
 }
