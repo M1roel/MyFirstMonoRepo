e0c961ac7c53aa5e63e53ae2a73954f3f7046495
M1roel <ppfautsch@yahoo.de>
2025-11-05 18:57:27 +0100

    feat: implement about section split component with dynamic image and text inputs

--- projects/atomic-components/src/lib/templates/split/about-section-split/about-section-split.component.ts
+++ projects/atomic-components/src/lib/templates/split/about-section-split/about-section-split.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TextComponent } from '../../../atoms/text/text.component';
+import { ImageComponent } from '../../../atoms/image/image.component';
+
+export interface AboutImage {
+  src: string;
+  alt: string;
+}
 
 @Component({
   selector: 'atomic-about-section-split',
-  imports: [],
+  imports: [CommonModule, TextComponent, ImageComponent],
   templateUrl: './about-section-split.component.html',
   styleUrl: './about-section-split.component.scss'
 })
 export class AboutSectionSplitComponent {
-
+  @Input() text: string = '';
+  @Input() images: AboutImage[] = [];
 }
