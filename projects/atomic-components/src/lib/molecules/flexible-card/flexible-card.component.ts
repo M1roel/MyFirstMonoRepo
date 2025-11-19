cf116084cd47661aeb7afd89fa56d69eae55422f
M1roel <ppfautsch@yahoo.de>
2025-11-19 18:48:29 +0100

    feat: add FlexibleCard component with HTML template and TypeScript class

---
+++ projects/atomic-components/src/lib/molecules/flexible-card/flexible-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'atomic-flexible-card',
+  imports: [],
+  templateUrl: './flexible-card.component.html',
+  styleUrl: './flexible-card.component.scss'
+})
+export class FlexibleCardComponent {
+
+}
