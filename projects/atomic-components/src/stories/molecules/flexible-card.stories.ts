0b679ddee98f0e3a405596fb7b24df2b939d4694
M1roel <ppfautsch@yahoo.de>
2025-11-20 16:41:16 +0100

    feat: update FlexibleCard styles and enhance Storybook stories with additional variants and components

--- projects/atomic-components/src/stories/molecules/flexible-card.stories.ts
+++ projects/atomic-components/src/stories/molecules/flexible-card.stories.ts
@@ -1,10 +1,19 @@
 import { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata } from '@storybook/angular';
 import { FlexibleCardComponent } from '../../lib/molecules/flexible-card/flexible-card.component';
+import { IconComponent } from '../../lib/atoms/icon/icon.component';
+import { HeadlineComponent } from '../../lib/atoms/headline/headline.component';
+import { TextComponent } from '../../lib/atoms/text/text.component';
 
 const meta: Meta<FlexibleCardComponent> = {
   title: 'Molecules/Flexible Card',
   component: FlexibleCardComponent,
   tags: ['autodocs'],
+  decorators: [
+    moduleMetadata({
+      imports: [IconComponent, HeadlineComponent, TextComponent],
+    }),
+  ],
   argTypes: {
     variant: {
       control: { type: 'select' },
@@ -20,10 +29,45 @@ export const BusinessVariant: Story = {
   args: {
     variant: 'business',
   },
+  render: (args) => ({
+    props: args,
+    template: `
+      <atomic-flexible-card [variant]="variant">
+        <atomic-headline 
+          card-mid
+          [headingText]="'Premium Business Solution'"
+          [level]="3">
+        </atomic-headline>
+        
+        <atomic-text 
+          card-down
+          [textContent]="'Professionelle Lösungen für Ihr Unternehmen mit erstklassigem Support und modernster Technologie.'">
+        </atomic-text>
+      </atomic-flexible-card>
+    `,
+  }),
 };
 
 export const PrimaryVariant: Story = {
   args: {
     variant: 'primary',
   },
+  render: (args) => ({
+    props: args,
+    template: `
+      <atomic-flexible-card [variant]="variant">
+        <atomic-headline 
+          card-mid
+          [headingText]="'User Favorite'"
+          [level]="3">
+        </atomic-headline>
+        
+        <atomic-text 
+          card-down
+          [textContent]="'Eine benutzerfreundliche Lösung, die von unseren Kunden geliebt wird und täglich genutzt wird.'"
+          [variant]="'primary'">
+        </atomic-text>
+      </atomic-flexible-card>
+    `,
+  }),
 };
