6ecfbb3eb01fe2509f5c8a46a8a55476f08d2d61
M1roel <ppfautsch@yahoo.de>
2025-11-21 15:38:13 +0100

    feat: enhance FlexCardWrapped and button components with new styles and heading support

--- projects/atomic-components/src/lib/templates/wrapped/flex-card-wrapped/flex-card-wrapped.component.ts
+++ projects/atomic-components/src/lib/templates/wrapped/flex-card-wrapped/flex-card-wrapped.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { FlexibleCardComponent } from '../../../molecules/flexible-card/flexible-card.component';
 import { ButtonComponent } from '../../../atoms/button/button.component';
+import { HeadlineComponent } from '../../../atoms/headline/headline.component';
 
 export interface CardData {
   topContent: string;
@@ -9,18 +10,19 @@ export interface CardData {
   bottomContent?: string;
   buttonLabel?: string;
   buttonAction?: () => void;
-  buttonVariant?: 'primary' | 'business';
-  variant?: 'primary' | 'business';
+  variant: 'primary' | 'business';
 }
 
 @Component({
   selector: 'atomic-flex-card-wrapped',
-  imports: [NgClass, FlexibleCardComponent, ButtonComponent],
+  imports: [NgClass, FlexibleCardComponent, ButtonComponent, HeadlineComponent],
   templateUrl: './flex-card-wrapped.component.html',
   styleUrl: './flex-card-wrapped.component.scss'
 })
 export class FlexCardWrappedComponent {
   @Input() variant: 'primary' | 'business' = 'primary';
   @Input() cards: CardData[] = [];
+  @Input() headingText: string = '';
+  @Input() headingLevel: number = 2;
 }
  
