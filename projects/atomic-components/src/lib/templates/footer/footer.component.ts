a3c06d97beaf3ba57529e54cacddb631c65dc8ea
M1roel <ppfautsch@yahoo.de>
2026-01-10 19:18:24 +0100

    feat: update footer component to include dynamic links for unternehmen and leistung

--- projects/atomic-components/src/lib/templates/footer/footer.component.ts
+++ projects/atomic-components/src/lib/templates/footer/footer.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FooterLinksComponent, FooterLink } from '../../molecules/footer/footer-links/footer-links.component';
 
 @Component({
   selector: 'atomic-footer',
-  imports: [],
+  imports: [FooterLinksComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  @Input() unternehmenLinks: FooterLink[] = [];
+  @Input() leistungLinks: FooterLink[] = [];
+  
 }
